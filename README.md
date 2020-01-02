1a1c59e696a3b647f7917271b7e062363fa7fb26
chenfengquan <chengfq@fusen.net.cn>
2020-01-02 09:20:40 +0800

    feat:   调整汇率查询接口

--- README.md
+++ README.md
@@ -8,3 +8,30 @@ git commit -m "first commit"
 git remote add origin https://github.com/nimdean/exchange-rate.git
 git push -u origin master
 ```
+
+
+## exchange-rate启动方法
+
+先克隆项目
+``` sh
+git clone https://github.com/nimdean/exchange-rate.git
+```
+打开项目文件安装依赖
+``` sh
+npm install
+```
+运行项目
+``` sh
+npm run dev
+```
+访问接口
+1. 浏览器访问<br/>
+  如查看`/rate`的数据，可打开浏览器直接输入`localhost:3000/rate`即可查看该接口返回的数据。
+2. 运用在前端项目中<br/>
+  一般使用插件`axios`来发送`ajax`请求。
+    ``` js
+    import axios from 'axios'
+    axios.get('/rate').then(rs => {
+      // rs为请求返回的内容
+    })
+    ```
--- routes/rate.js
+++ routes/rate.js
@@ -6,53 +6,44 @@ const cheerio = require("cheerio")
 router.prefix('/rate')
 
 router.get('/', async (ctx,next) => {
-  let response1 = await axios({
-    url:'https://srh.bankofchina.com/search/whpj/search_cn.jsp',
-    method: 'post',
-    params: {
-      pjname: '美元'
+  let rateInfo = async params => {
+    try{
+      let response = await axios({
+        url: 'https://srh.bankofchina.com/search/whpj/search_cn.jsp',
+        method: 'post',
+        params
+      })
+      const $ = cheerio.load(response.data)
+      let obj = {}
+      let thArr = $('.BOC_main table tr').eq(0)
+      let tdArr = $('.BOC_main table tr').eq(1)
+      for(let i = 0;i < thArr.find('th').length;i ++){
+        let temp1 = thArr.find('th')[i].children[0].data
+        let temp2 = tdArr.find('td')[i].children[0].data
+        if(temp1 === '货币名称'){
+          obj.currency = {'美元':'USD','港币':'HKD'}[temp2]
+        }
+        if(['现汇买入价','现汇卖出价','中行折算价'].includes(temp1)){
+          obj[{'现汇买入价':'buyingRate','现汇卖出价':'sellingRate','中行折算价':'exchangeRate'}[temp1]] = temp2*1
+        }
+        if(temp1 === '发布时间'){
+          obj.updateTime = temp2
+        }
+        obj.centralParity = ((obj.buyingRate + obj.sellingRate)/2).toFixed(2)*1
+      }
+      return obj
+    }catch(err){
+      return
     }
-  })
-  const $1 = cheerio.load(response1.data)
-  let obj1 = {}
-  let thArr1 = $1('.BOC_main table tr').eq(0)
-  let tdArr1 = $1('.BOC_main table tr').eq(1)
-  for(let i = 0;i < thArr1.find('th').length;i ++){
-    let temp = thArr1.find('th')[i].children[0].data
-    obj1[temp] = isNaN(tdArr1.find('td')[i].children[0].data) ? tdArr1.find('td')[i].children[0].data : tdArr1.find('td')[i].children[0].data*1
   }
-  let response2 = await axios({
-    url:'https://srh.bankofchina.com/search/whpj/search_cn.jsp',
-    method: 'post',
-    params: {
-      pjname: '港币'
-    }
-  })
-  const $2 = cheerio.load(response2.data)
-  let obj2 = {}
-  let thArr2 = $2('.BOC_main table tr').eq(0)
-  let tdArr2 = $2('.BOC_main table tr').eq(1)
-  for(let i = 0;i < thArr2.find('th').length;i ++){
-    let temp = thArr2.find('th')[i].children[0].data
-    obj2[temp] = isNaN(tdArr2.find('td')[i].children[0].data) ? tdArr2.find('td')[i].children[0].data : tdArr2.find('td')[i].children[0].data*1
-  }
-  let arr = [{
-    currency: 'USD',
-    exchangeRate: obj1['中行折算价'],
-    buyingRate: obj1['现汇买入价'],
-    sellingRate: obj1['现汇卖出价'],
-    centralParity: ((obj1['现汇买入价']+obj1['现汇卖出价'])/2).toFixed(2)*1
-  }, {
-    currency: 'HKD',
-    exchangeRate: obj2['中行折算价'],
-    buyingRate: obj2['现汇买入价'],
-    sellingRate: obj2['现汇卖出价'],
-    centralParity: ((obj2['现汇买入价']+obj2['现汇卖出价'])/2).toFixed(2)*1
-  }]
   ctx.body = {
     code: 200,
     status: 'success',
-    data: arr
+    data: [await rateInfo({
+      pjname: '美元'
+    }), await rateInfo({
+      pjname: '港币'
+    })]
   }
 })
 
